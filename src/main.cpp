770d63f26e5146e036f55dc867f60fe045da5d6e
cuongnt02 <cuong.ntc02@gmail.com>
2024-12-04 00:47:33 +0700

    loaded pixel data to an array

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
+#include <cstddef>
 #include <iostream>
 #include <Magick++.h>
+#include <vector>
 
 
 
@@ -22,16 +24,30 @@ int main(int argc, char** argv) {
 
     image.modifyImage();
 
+    std::vector<std::vector<std::vector<int>>> pixelArray(
+        w,
+        std::vector<std::vector<int>>(h,
+            std::vector<int>(3)
+        )
+    );
 
-    for (int i = 0; i <= w; ++i) {
-        for (int j = 0; j <= h; ++j) {
+    for (std::size_t i{ 0 }; i < w; ++i) {
+        for (std::size_t j { 0 }; j < h; ++j) {
             Magick::ColorRGB px = image.pixelColor(i, j);
-            std::cout << "Pixel at point (" << i << ", " << j << "): ";
-            std::cout << "( " << px.red() * 255.0 
-                << ", " << px.green() * 255.0 
-                << ", " << px.blue() * 255.0 <<" )" << std::endl;
+            pixelArray[i][j][0] = px.red()*255;
+            pixelArray[i][j][1] = px.green()*255;
+            pixelArray[i][j][2] = px.blue()*255;
         }
     }
+
+    std::cout << "Successfully contructed pixel matrix!" << std::endl;
+    std::cout << "Pixel matrix size: " << w << " x " << h << std::endl; 
+    std::cout << "Iterating through pixel contents: " << std::endl;
+    std::cout << "Pointer here\n";
+    for (std::size_t i{ 0 }; i < pixelArray.size(); ++i)
+        for (std::size_t j{ 0 }; j < pixelArray[i].size(); ++j) {
+            std::cout << "(" << pixelArray[i][j][0] << ", " << pixelArray[i][j][1] << ", " << pixelArray[i][j][2] << ")" << std::endl;
+        }
 
 
     return 0;
